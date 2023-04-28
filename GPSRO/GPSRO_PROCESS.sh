3b9d5a5f9ae4acadfbf6753a59bcc39400b6fd69
Swapan Mallick <sm09722@login2.juno.cmcc.scc>
2023-04-28 11:42:03 +0200

    GPSRO files extended with *.bufr

--- GPSRO/GPSRO_PROCESS.sh
+++ GPSRO/GPSRO_PROCESS.sh
@@ -16,14 +16,12 @@ MM=`echo $EVENT | cut -c5-6`
 #
 cd ${YYYY}/
 #--------------------------------------
-for DD in 09; do
-##for DD in {10..28}; do
+for DD in {10..29}; do
 #
 if [ ${DD} -ge 11 ]; then
   DD1=$((DD-1))
 else
-  DD1=08
-  ##DD1='0'$((DD-1))
+  DD1='0'$((DD-1))
 fi
 # 
 #DD1=04 # Previous day
@@ -41,26 +39,26 @@ for UTC in 00 06 12 18 ; do
    if [ ${UTC} = 00 ];then
       echo ${UTC}
       cat bfr_${DATEP}_21* bfr_${DATEP}_22* bfr_${DATEP}_23* bfr_${DATE}_00* bfr_${DATE}_01* bfr_${DATE}_02* > GPSRO_${UTC}
-      rm -rf  ${OUTPUT}/${YYYY}/${MM}/${DD}/GPSRO_${UTC}00
-      cp -f  GPSRO_${UTC} ${OUTPUT}/${YYYY}/${MM}/${DD}/GPSRO_${UTC}00
+      rm -rf  ${OUTPUT}/${YYYY}/${MM}/${DD}/GPSRO_${UTC}00.bufr
+      cp -f  GPSRO_${UTC} ${OUTPUT}/${YYYY}/${MM}/${DD}/GPSRO_${UTC}00.bufr
       mv GPSRO_${UTC} GPSRO_${UTC}_${DD}_${MM}_${YYYY}
    elif [ ${UTC} = 06 ];then
       echo ${UTC}
       cat bfr_${DATE}_03* bfr_${DATE}_04* bfr_${DATE}_05* bfr_${DATE}_06* bfr_${DATE}_07* bfr_${DATE}_08* > GPSRO_${UTC}
-      rm -rf  ${OUTPUT}/${YYYY}/${MM}/${DD}/GPSRO_${UTC}00
-      cp -f  GPSRO_${UTC} ${OUTPUT}/${YYYY}/${MM}/${DD}/GPSRO_${UTC}00
+      rm -rf  ${OUTPUT}/${YYYY}/${MM}/${DD}/GPSRO_${UTC}00.bufr
+      cp -f  GPSRO_${UTC} ${OUTPUT}/${YYYY}/${MM}/${DD}/GPSRO_${UTC}00.bufr
       mv GPSRO_${UTC} GPSRO_${UTC}_${DD}_${MM}_${YYYY}
    elif [ ${UTC} = 12 ];then
       echo ${UTC}
       cat bfr_${DATE}_09* bfr_${DATE}_10* bfr_${DATE}_11* bfr_${DATE}_12* bfr_${DATE}_13* bfr_${DATE}_14* > GPSRO_${UTC}
-      rm -rf  ${OUTPUT}/${YYYY}/${MM}/${DD}/GPSRO_${UTC}00
-      cp -f  GPSRO_${UTC} ${OUTPUT}/${YYYY}/${MM}/${DD}/GPSRO_${UTC}00
+      rm -rf  ${OUTPUT}/${YYYY}/${MM}/${DD}/GPSRO_${UTC}00.bufr
+      cp -f  GPSRO_${UTC} ${OUTPUT}/${YYYY}/${MM}/${DD}/GPSRO_${UTC}00.bufr
       mv GPSRO_${UTC} GPSRO_${UTC}_${DD}_${MM}_${YYYY}
    elif [ ${UTC} = 18 ];then
       echo ${UTC}
       cat bfr_${DATE}_15* bfr_${DATE}_16* bfr_${DATE}_17* bfr_${DATE}_18* bfr_${DATE}_19* bfr_${DATE}_20* > GPSRO_${UTC}
-      rm -rf  ${OUTPUT}/${YYYY}/${MM}/${DD}/GPSRO_${UTC}00
-      cp -f  GPSRO_${UTC} ${OUTPUT}/${YYYY}/${MM}/${DD}/GPSRO_${UTC}00
+      rm -rf  ${OUTPUT}/${YYYY}/${MM}/${DD}/GPSRO_${UTC}00.bufr
+      cp -f  GPSRO_${UTC} ${OUTPUT}/${YYYY}/${MM}/${DD}/GPSRO_${UTC}00.bufr
       mv GPSRO_${UTC} GPSRO_${UTC}_${DD}_${MM}_${YYYY}
    fi
 ##
